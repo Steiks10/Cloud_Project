970eb59aed424014345c668638dbf0124e2bcb4a
Marco-Castro01 <86668466+Marco-Castro01@users.noreply.github.com>
2024-05-07 18:42:40 -0400

    Fix credentials

--- angular-cc-1-server/dbConection.js
+++ angular-cc-1-server/dbConection.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'clothing_store',
-    password: 'marge25-30',
+    password: 'password',
     port: 5432,
 });
 
