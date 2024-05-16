397dca9d2af7384a5be234409b14f426c903f1c0
steiks10 <steiker.m2002@gmail.com>
2024-05-15 22:43:34 -0400

    [FIX]

---
+++ angular-cc-1-server/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    dbConfig: {
+        user: 'postgres',
+        host: '10.70.203.34',
+        database: 'clothing_store',
+        password: 'admin',
+        port:  5432,
+    }
+};
