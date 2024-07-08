f0636b73dd6ee8069dd65a355ea4a3ce99e8eac7
Marco-Castro01 <castroo8a2@gmail.com>
2024-07-08 10:59:24 -0400

    [FIX] modify db user

--- angular-cc-1-server/config.js
+++ angular-cc-1-server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     dbConfig: {
-        user: 'db-1',
+        user: 'postgres',
         host: process.env.DB_HOST, // Nombre del servicio de la base de datos
         database: 'clothing_store',
         password: 'db-1',
